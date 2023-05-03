6a7ce6fb100798f6f2547c1f01baa1c88f029924
Madz No <madzno12@gmail.com>
2023-05-03 09:28:57 -0400

    updating exercises

---
+++ tqm/child_nodes.js
@@ -0,0 +1,76 @@
+/*
+Element 1
+Direct Elements - h1 (element 2), p (element 4), a (element 6), div (element 8)
+Direct Text - space between div and h1, space between h1 and p, space before closing
+p, space prior to a, space prior to last div
+(Direct = 9)
+(Indirect = 12)
+
+Element 2
+Direct Elements - em
+Direct Text - 'Hello,'
+Indirect Text - 'World'
+(Direct = 2)
+(Indirect = 1)
+
+Element 3
+Direct elements - text 'World'
+Indirect - 0
+(Direct = 1)
+(Indirect = 0)
+
+Element 4
+Direct Elements - span
+Direct Text- 'Welcome to wonderland. This is an., and 'place'
+Indirect Elements - 0
+Indirect Text - 'awesome'
+(Direct = 3)
+(Indirect = 1)
+
+Element 5
+Direct Element - 0
+Direct Text - 'awesome'
+Indirect - 0
+(Direct = 1)
+(Indirect = 0)
+
+Element 6
+Direct Element - strong
+Direct Text - 0
+Indirect Element - 0
+Indirect Text - 'Enter'
+(Direct = 1)
+(Indirect = 1)
+
+Element 7
+Direct Element - 0
+Direct Text - 'Enter'
+Indirect Element - 0
+Indirect Text - 0
+(Direct = 1)
+(Indirect = 0)
+
+Element 8
+Direct Element - p
+Direct Text - 0
+Indirect Element - a
+Indirect Text - 'Go back'
+(Direct = 1)
+(Indirect = 2)
+
+Element 9
+Direct Element - a
+Direct Text - 0
+Indirect Element - 0
+Indirect Text - 'Go back'
+(Direct = 1)
+(INdirect = 1)
+
+Element 10
+Direct Element - 0
+Direct Text "Go back"
+Indirect Element/Text - 0
+(Direct = 1)
+(Indirect = 0)
+*/
+
---
+++ tqm/counting_nodes.js
@@ -0,0 +1,12 @@
+/*
+1. elements - Html, head, body, div, em, paragraph
+2. whitespace - between div and p, between p and front of line
+3. text - 'then press the', 'draw', 'button'
+11
+
+1. elements = html, head, body, div, em, paragraph
+2. no whitespace
+3. text - then press the, draw, button.
+9
+
+*/
