2553e360edbb0c98a6750d0f9506901d95e575a8
Madz No <madzno12@gmail.com>
2023-05-07 15:17:34 -0400

    adding tqm exercises

---
+++ tqm/html_imaging.js
@@ -0,0 +1,21 @@
+< !doctype html >
+  <html>
+    <head>
+      <style type='text/css'>
+        .emphasis {
+          'font-weight': 'bold',
+        },
+        .light {
+          'color': 'gray',
+        },
+
+      </style>
+      <title>HTML Imaging</title>
+    </head>
+    <body>
+      <header id='header'>
+        <h1 class='emphasis light'>Dynamic Content</h1>
+        <p>Hello World!</p>
+      </header>
+    </body>
+  </html>
