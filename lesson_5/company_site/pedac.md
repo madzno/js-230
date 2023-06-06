7113aeaea76ca0d3f6b0ca43ae3b9c4593432a1d
Madz No <madzno12@gmail.com>
2023-06-06 13:36:16 -0400

    adding company site project

---
+++ lesson_5/company_site/pedac.md
@@ -0,0 +1,7 @@
+- add event listner for a click event on the a.close element tha tis a child of the '#modal div'
+- prevent default
+- call hide modal
+
+- function: hide modal
+
+- toggle the hide and show classes on $layerDiv and $modalDiv
--- lesson_5/company_site/team_modal.js
+++ lesson_5/company_site/team_modal.js
@@ -1,43 +1,75 @@
-$(function () {
-  let $layerDiv = $('#modal-layer');
-  let $modalDiv = $('#modal');
+class Modal {
+  constructor() {
+    this.$layerDiv = $('#modal-layer');
+    this.$modalDiv = $('#modal');
+    this.$h3 = $('#modal > h3');
+    this.$p = $('#modal > p');
+    this.$teamImg = $('#modal > img');
+    this.$list = $('#team-members');
+    this.$closeLink = $('a.close');
 
-  function showModal(link) {
-    $layerDiv.toggleClass('hide');
-    $layerDiv.toggleClass('show');
+    this.bindEvents();
+  }
 
-    $modalDiv.toggleClass('hide');
-    $modalDiv.toggleClass('show');
+  bindEvents() {
+    this.$list.on('click', 'a', function (event) {
+      event.preventDefault();
+      let $link;
 
-    let $h3 = $('#modal > h3');
+      if (event.target.tagName === 'A') {
+        $link = $(event.target);
+      } else {
+        $link = $(event.target.parentNode);
+      }
+
+      this.showModal($link);
+    }.bind(this));
+
+    this.$closeLink.on('click', function (event) {
+      event.preventDefault();
+      this.hideModal();
+    }.bind(this))
+
+  }
+
+  toggle() {
+    this.$layerDiv.toggleClass('hide');
+    this.$layerDiv.toggleClass('show');
+
+    this.$modalDiv.toggleClass('hide');
+    this.$modalDiv.toggleClass('show');
+  }
+
+  showModal(link) {
+    this.toggle();
+
     let name = link.attr('data-name');
-    $h3.text(`${name}`);
+    this.$h3.text(`${name}`);
 
-    let $p = $('#modal > p');
     let info = link.attr('data-text');
-    $p.text(`${info}`);
+    this.$p.text(`${info}`);
 
-    let $teamImg = $('#modal > img');
     let imgLink = link.attr('data-image-source');
-    $teamImg.attr({
+
+    this.$teamImg.attr({
       src: `${imgLink}`,
       alt: `${name}`
     });
   }
 
-  let $list = $('#team-members');
+  hideModal() {
+    this.toggle();
+    this.$h3.text('');
+    this.$p.text('');
+    this.$teamImg.attr({
+      src: '',
+      alt: ''
+    });
+  }
+}
 
-  $list.on('click', 'a', function (event) {
-    event.preventDefault();
-    let $link;
+$(function () {
 
-    if (event.target.tagName === 'A') {
-      $link = $(event.target);
-    } else {
-      $link = $(event.target.parentNode);
-    }
-
-    showModal($link);
-  });
+  let modalManager = new Modal();
 
 });
